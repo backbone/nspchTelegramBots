d1b9abe2348d2f96340c7068c95f39d225d591fd
Kolan Sh <backbone@backbone.ws>
2022-01-03 00:30:25 +0300

    is in progress...

--- nspchInterviewBot/interview.py
+++ nspchInterviewBot/interview.py
@@ -47,8 +47,44 @@ class Form(StatesGroup):
 
 # Answers
 class Answers():
-    back_to_gegin_txt = "Вернуться к началу беседы"
-    i_looked_video_txt = "Я просмотрел презентацию"
+    back_to_gegin_answ = "Вернуться к началу беседы"
+    i_looked_video_answ = "Я просмотрел презентацию"
+    i_opened_phone_answ = "Я открыл номер телефона"
+    yes_answ = "Да"
+    no_answ = "Нет"
+    tiktok_answ = "Tik-Tok"
+    telegram_answ = "Telegram"
+    vkontakte_answ = "Vkontakte"
+    instagram_answ = "Instagram"
+    lt_10_answ = "<10"
+    in_10_20_answ = "10-20"
+    in_20_50_answ = "20-50"
+    gt_50_answ = ">50"
+    time_00_06_answ = "0:00-6:00"
+    time_06_12_answ = "6:00-12:00"
+    time_12_18_answ = "12:00-18:00"
+    time_18_24_answ = "18:00-24:00"
+    age_gt_16_answ = "есть 16 лет"
+    age_lt_16_answ = "младше 16 лет"
+    prof_it_answ = "IT/Dev/Web"
+    prof_jurist_answ = "Юрист"
+    prof_mark_answ = "Маркетолог"
+    prof_book_keeper_answ = "Бухгалтер"
+    prof_smm_answ = "SMM-специалист"
+    prof_journalist_answ = "Журналист"
+    prof_video_editor_answ = "Аудио/Видео-монтаж"
+    prof_speaker_answ = "Диктор"
+    prof_teacher_answ = "Педагог"
+    salary_1536_answ = "1536€ / мес. (МРОТ 9.6€/ч * 8ч. * 20раб.дн.)"
+    salary_3840_answ = "3840€ / мес. (МРОТ 9.6€/ч * 20ч. * 20раб.дн.)"
+    salary_5952_answ = "5952€ / мес. (МРОТ 9.6€/ч * 20ч. * 31раб.дн.)"
+    hours_1_answ = "1ч./день"
+    hours_2_answ = "2ч./день"
+    hours_4_answ = "4ч./день"
+    hours_8_answ = "8ч./день"
+    hours_16_answ = "16ч./день"
+    hours_20_answ = "20ч./день"
+    other_answ = "Прочее"
 
 # You can use state '*' if you need to handle all states
 @dp.message_handler(state='*', commands='cancel')
@@ -75,7 +111,7 @@ async def cmd_start(message: types.Message):
 в размере 0.34% от ЗП.")
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-    markup.add(Answers.i_looked_video_txt, Answers.back_to_gegin_txt)
+    markup.add(Answers.i_looked_video_answ, Answers.back_to_gegin_answ)
 
     video_path = 'data/Greeting/Greeting-'+str(random.randint(0,3))+'.mp4'
     await bot.send_video(message.chat.id, open(video_path, 'rb'),
@@ -89,19 +125,19 @@ def get_voice(s="001"):
     return 'data/voice/chnv-001/'+str(datetime.datetime.now().hour+1)+'/'+s+'.mp3'
 
 async def check_reset(message):
-    if message.text == Answers.back_to_gegin_txt:
+    if message.text == Answers.back_to_gegin_answ:
         await Form.stateBegin.set()
         await cmd_start(message)
 
 @dp.message_handler(lambda message: message.text not in [
-    Answers.i_looked_video_txt, Answers.back_to_gegin_txt], state=Form.stateBegin)
+    Answers.i_looked_video_answ, Answers.back_to_gegin_answ], state=Form.stateBegin)
 async def process_begin_invalid(message: types.Message):
     return await message.reply("Выберите вариант с экранной клавиатуры.")
 
 @dp.message_handler(state=Form.stateBegin)
 async def process_begin(message: types.Message):
     await check_reset(message)
-    if message.text == Answers.i_looked_video_txt:
+    if message.text == Answers.i_looked_video_answ:
         markup = types.ReplyKeyboardRemove()
         #await process_closed_number(message)
         await bot.send_voice(message.chat.id, open(get_voice('004'), 'rb'),
@@ -115,8 +151,8 @@ async def process_begin(message: types.Message):
 #    if check_reset(message): return
 #    await message.reply("Глаз Бога пока не работает (не вижу, открыт ли номер)...")
 
-#@dp.message_handler(state=Form.stateSocialNetworkQ)
-#async def process_social_network_q(message: types.Message):
+@dp.message_handler(state=Form.stateSocialNetworkQ)
+async def process_social_network_q(message: types.Message):
 
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
