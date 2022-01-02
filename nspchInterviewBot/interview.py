4af76fe97794e362a786fc5b5a5ecafeb412088d
Kolan Sh <backbone@backbone.ws>
2022-01-02 20:05:54 +0300

    is in progress...

--- nspchInterviewBot/interview.py
+++ nspchInterviewBot/interview.py
@@ -8,7 +8,7 @@ from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ParseMode
 from aiogram.utils import executor
-import random
+import random, datetime
 
 logging.basicConfig(level=logging.INFO)
 
@@ -50,23 +50,24 @@ class Form(StatesGroup):
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
 
-    await message.reply("Рады поприветствовать в ПрофСоюзе Правозащитников \
-            без границ!\nПрофсоюз является экстерриториальным работодателем \
-            с профсоюзным взносом в размере 0.34% от ЗП.")
+    #await message.reply("Рады поприветствовать в ПрофСоюзе Правозащитников \
+            #        без границ!\nПрофсоюз является экстерриториальным работодателем \
+            #с профсоюзным взносом в размере 0.34% от ЗП.")
 
 
-    video_path = 'data/Greeting/Greeting-'+str(random.randint(0,3))+'.mp4'
-    await bot.send_video(message.chat.id, open(video_path, 'rb'),
-                         caption="Просмотрите ознакомительную видеопрезентацию.")
+    #video_path = 'data/Greeting/Greeting-'+str(random.randint(0,3))+'.mp4'
+    #await bot.send_video(message.chat.id, open(video_path, 'rb'),
+    #                     caption="Просмотрите ознакомительную видеопрезентацию.")
 
 
-    """
-    Conversation's entry point
-    """
-    # Set state
-    await Form.name.set()
+    #"""
+    #Conversation's entry point
+    #"""
+    ## Set state
+    await Form.stateBegin.set()
+    await process_begin(message)
 
-    await message.reply("Hi there! What's your name?")
+    #await message.reply("Hi there! What's your name?")
 
 
 # You can use state '*' if you need to handle all states
@@ -87,6 +88,12 @@ async def cancel_handler(message: types.Message, state: FSMContext):
     await message.reply('Cancelled.', reply_markup=types.ReplyKeyboardRemove())
 
 @dp.message_handler(state=Form.stateBegin)
+async def process_begin(message: types.Message):
+    audio_path = 'data/voice/chnv-001/'+str(datetime.datetime.now().hour+1)+'/001.mp3'
+    await bot.send_voice(message.chat.id, open(audio_path, 'rb'),
+                         caption="Рады поприветствовать в ПрофСоюзе Правозащитников \
+            без границ!\nПрофсоюз является экстерриториальным работодателем \
+            с профсоюзным взносом в размере 0.34% от ЗП.")
 
     #await message.reply("Рады поприветствовать в ПрофСоюзе Правозащитников \
     #        без границ!\nПрофсоюз является экстерриториальным работодателем \
@@ -98,72 +105,72 @@ async def cancel_handler(message: types.Message, state: FSMContext):
                          caption="Просмотрите ознакомительную видеопрезентацию.")
     Form.next()
 
-@dp.message_handler(state=Form.name)
-async def process_name(message: types.Message, state: FSMContext):
-    """
-    Process user name
-    """
-    async with state.proxy() as data:
-        data['name'] = message.text
-
-    await Form.next()
-    await message.reply("How old are you?")
-
-
+#@dp.message_handler(state=Form.name)
+#async def process_name(message: types.Message, state: FSMContext):
+#    """
+#    Process user name
+#    """
+#    async with state.proxy() as data:
+#        data['name'] = message.text
+#
+#    await Form.next()
+#    await message.reply("How old are you?")
+#
+#
 # Check age. Age gotta be digit
-@dp.message_handler(lambda message: not message.text.isdigit(), state=Form.age)
-async def process_age_invalid(message: types.Message):
-    """
-    If age is invalid
-    """
-    return await message.reply("Age gotta be a number.\nHow old are you? (digits only)")
+#@dp.message_handler(lambda message: not message.text.isdigit(), state=Form.age)
+#async def process_age_invalid(message: types.Message):
+#    """
+#    If age is invalid
+#    """
+#    return await message.reply("Age gotta be a number.\nHow old are you? (digits only)")
 
 
-@dp.message_handler(lambda message: message.text.isdigit(), state=Form.age)
-async def process_age(message: types.Message, state: FSMContext):
-    # Update state and data
-    await Form.next()
-    await state.update_data(age=int(message.text))
-
-    # Configure ReplyKeyboardMarkup
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-    markup.add("Male", "Female")
-    markup.add("Other")
-
-    await message.reply("What is your gender?", reply_markup=markup)
-
-
-@dp.message_handler(lambda message: message.text not in ["Male", "Female", "Other"], state=Form.gender)
-async def process_gender_invalid(message: types.Message):
-    """
-    In this example gender has to be one of: Male, Female, Other.
-    """
-    return await message.reply("Bad gender name. Choose your gender from the keyboard.")
+#@dp.message_handler(lambda message: message.text.isdigit(), state=Form.age)
+#async def process_age(message: types.Message, state: FSMContext):
+#    # Update state and data
+#    await Form.next()
+#    await state.update_data(age=int(message.text))
+#
+#    # Configure ReplyKeyboardMarkup
+#    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+#    markup.add("Male", "Female")
+#    markup.add("Other")
+#
+#    await message.reply("What is your gender?", reply_markup=markup)
 
 
-@dp.message_handler(state=Form.gender)
-async def process_gender(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['gender'] = message.text
-
-        # Remove keyboard
-        markup = types.ReplyKeyboardRemove()
-
-        # And send message
-        await bot.send_message(
-            message.chat.id,
-            md.text(
-                md.text('Hi! Nice to meet you,', md.bold(data['name'])),
-                md.text('Age:', md.code(data['age'])),
-                md.text('Gender:', data['gender']),
-                sep='\n',
-            ),
-            reply_markup=markup,
-            parse_mode=ParseMode.MARKDOWN,
-        )
-
-    # Finish conversation
-    await state.finish()
+#@dp.message_handler(lambda message: message.text not in ["Male", "Female", "Other"], state=Form.gender)
+#async def process_gender_invalid(message: types.Message):
+#    """
+#    In this example gender has to be one of: Male, Female, Other.
+#    """
+#    return await message.reply("Bad gender name. Choose your gender from the keyboard.")
+#
+#
+#@dp.message_handler(state=Form.gender)
+#async def process_gender(message: types.Message, state: FSMContext):
+#    async with state.proxy() as data:
+#        data['gender'] = message.text
+#
+#        # Remove keyboard
+#        markup = types.ReplyKeyboardRemove()
+#
+#        # And send message
+#        await bot.send_message(
+#            message.chat.id,
+#            md.text(
+#                md.text('Hi! Nice to meet you,', md.bold(data['name'])),
+#                md.text('Age:', md.code(data['age'])),
+#                md.text('Gender:', data['gender']),
+#                sep='\n',
+#            ),
+#            reply_markup=markup,
+#            parse_mode=ParseMode.MARKDOWN,
+#        )
+#
+#    # Finish conversation
+#    await state.finish()
 
 
 if __name__ == '__main__':
